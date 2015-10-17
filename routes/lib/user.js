c005534b121ba1299b711a5859bb5f154555d0e8
Evan Turner <evturn@gmail.com>
2015-10-16 22:44:04 -0400

    Create climate attribute for user

--- routes/lib/user.js
+++ routes/lib/user.js
@@ -19,6 +19,18 @@ let userSchema = new mongoose.Schema({
     long     : {type: String},
     lastSeen : {type: String},
     vicinity : {type: String}
+  },
+  climate: {
+    vicinity    : {type: String},
+    temp        : {type: String},
+    humidity    : {type: String},
+    high        : {type: String},
+    low         : {type: String},
+    description : {type: String},
+    sunrise     : {type: String},
+    sunset      : {type: String},
+    country     : {type: String},
+    wind        : {type: String}
   }
 });
 
