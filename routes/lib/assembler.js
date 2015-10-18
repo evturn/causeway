9bb9f67edbdbe18faff12eef2815149a96c1a445
Evan Turner <evturn@gmail.com>
2015-10-18 17:07:26 -0400

    Create groups and group object

--- routes/lib/assembler.js
+++ routes/lib/assembler.js
@@ -21,8 +21,22 @@ let site = {
       users: users,
       transactions: transactions,
       userCount: 3
+    },
+    {
+      _id: '4254ce4ce4cfcd16f55cfa3d',
+      name: 'Montreal',
+      users: users,
+      transactions: transactions,
+      userCount: 3
     }
   ],
+  group: {
+    _id: '4252ce4ce4cfcd16f55cfa3d',
+    name: 'Thug Nation',
+    users: users,
+    transactions: transactions,
+    userCount: 3
+  },
   components: components,
   pages: [
     'now',
